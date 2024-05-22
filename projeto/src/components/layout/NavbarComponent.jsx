cb78a84fb32df450e5cb2ccce4077641cfa96422
Ana Carolina <caroltorresact@gmail.com>
2024-05-22 18:30:29 +0100

    add navbar

--- projeto/src/components/layout/NavbarComponent.jsx
+++ projeto/src/components/layout/NavbarComponent.jsx
@@ -1,19 +1,16 @@
 import styles from "./NavBar.module.css";
 import Link from "next/link";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 import { RiHome3Line } from "react-icons/ri";
 import { HomeIcon } from "../icons/HomeIcon";
+import { PassportIcon } from "../icons/PassportIcon";
 import { useState } from "react";
 
-
 export function NavBar() {
-  const notify = () => toast("Link clicked!");
   const [hoveredItem, setHovered] = useState(null);
 
   return (
     <div className={styles.container}>
-      <Link href="/home" className={styles.button} onClick={notify}>
+      <Link href="/home" className={styles.button} >
         <div
           onMouseEnter={() => setHovered("home")}
           onMouseOut={() => setHovered(null)}
@@ -23,33 +20,46 @@ export function NavBar() {
         </div>
       </Link>
 
-      <Link href="/community" className={styles.button} onClick={notify}>
+      <Link href="/community" className={styles.button} >
         <div
           onMouseEnter={() => setHovered("community")}
           onMouseOut={() => setHovered(null)}
         >
-          <HomeIcon color={hoveredItem === "community" ? "blue" : "red"} />
-          <Community></Community>
+          <PassportIcon color={hoveredItem === "community" ? "blue" : "red"} />
+          Community
         </div>
       </Link>
 
-      <Link href="/app/community" className={styles.button}>
-        <img src="/public/icons/community-icon.png" alt="Community Icon" />
+      <Link href="/passport" className={styles.button} >
+        <div
+          onMouseEnter={() => setHovered("passport")}
+          onMouseOut={() => setHovered(null)}
+        >
+          <PassportIcon color={hoveredItem === "passport" ? "white" : "red"} />
+          Passport
+        </div>
       </Link>
 
-      <Link href="/app/passport" className={styles.button} onClick={notify}>
-        <img src="/public/icons/passport-icon.png" alt="Passport Icon" />
+      <Link href="/countries" className={styles.button} >
+        <div
+          onMouseEnter={() => setHovered("countries")}
+          onMouseOut={() => setHovered(null)}
+        >
+          <HomeIcon color={hoveredItem === "countries" ? "blue" : "red"} />
+          Countries
+        </div>
       </Link>
 
-      <Link href="/app/global" className={styles.button} onClick={notify}>
-        <img src="/public/icons/global-icon.png" alt="Global Icon" />
+      <Link href="/profile" className={styles.button} >
+        <div
+          onMouseEnter={() => setHovered("profile")}
+          onMouseOut={() => setHovered(null)}
+        >
+          <HomeIcon color={hoveredItem === "profile" ? "blue" : "red"} />
+          Profile
+        </div>
       </Link>
 
-      <Link href="/app/profile" className={styles.button} onClick={notify}>
-        <img src="/public/icons/profile-icon.png" alt="Profile Icon" />
-      </Link>
-
-      <ToastContainer />
     </div>
   );
 }
