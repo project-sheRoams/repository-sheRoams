8c4135861048beeda510bd53ecc85e22e760907b
Ana Carolina <caroltorresact@gmail.com>
2024-05-27 18:06:19 +0100

    Changed file name

--- projeto/src/pages/Home.js
+++ projeto/src/pages/Home.js
@@ -2,7 +2,7 @@ import { Header } from "@/components/layout/Header";
 import { NavBar } from "@/components/layout/NavbarComponent";
 import { Search } from "@/components/layout/SearchForm";
 import Loading from "@/components/layout/Loading";
-import { useState, useEffect } from "react"
+import { useState, useEffect } from "react";
 
 const LoginPage = () => {
   // const [currentSearch, setCurrentSearch] = useState("");
